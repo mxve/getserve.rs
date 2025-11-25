952171f2218412bf324d570c89d75dd1152234d8
mxve <68632137+mxve@users.noreply.github.com>
2025-11-25 19:00:31 +0100

    fix oopsie

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../dist')));
